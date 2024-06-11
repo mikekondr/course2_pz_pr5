f226ae2d718040dcce3e6b33bba6fd463850ef46
mikekondr <mike.kondr@gmail.com>
2024-06-12 02:06:16 +0300

    init

---
+++ task_8/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace task_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\tВведіть дані ТВ");
+        Console.Write("Виробник: "); string? v = Console.ReadLine();
+        Console.Write("Модельний ряд: "); string? s = Console.ReadLine();
+        Console.Write("Модель: "); string? m = Console.ReadLine();
+        Console.Write("Серійний номер: "); string? sn = Console.ReadLine();
+        decimal d = getDecimal("Діагональ екрану: ");
+        decimal p = getDecimal("Ціна телевізора: ");
+
+        Console.Write("Чи має ТВ приймач Т2? (y - так, n - ні): ");
+        ConsoleKeyInfo t = Console.ReadKey();
+        Console.WriteLine();
+        Console.Write("Чи має ТВ вхід HDMI? (y - так, n - ні): ");
+        ConsoleKeyInfo h = Console.ReadKey();
+        
+        TV myTV = new TV(v, s, m, sn, d,
+            (h.Key == ConsoleKey.Y), (t.Key == ConsoleKey.Y), p);
+
+        Console.WriteLine("\n\n");
+        Console.WriteLine("Інформація про ТВ:");
+        Console.WriteLine(myTV.getFullName());
+        Console.WriteLine(myTV.getDescription());
+        Console.WriteLine($"Ціна за дюйм: {myTV.getPricePerInch()}");
+    }
+
+    private static decimal getDecimal(string s)
+    {
+        decimal d = 0;
+        while (d == 0)
+        {
+            Console.Write(s);
+            string? str = Console.ReadLine();
+            decimal.TryParse(str, out d);
+            if (d == 0 || d < 0)
+            {
+                Console.WriteLine("ПОМИЛКА! Значення може бути тільки " +
+                    "раціональним числом більше нуля у вигляді десяткового дробу!");
+            }
+        }
+        return d;
+    }
+}
+
---
+++ task_8/TV.cs
@@ -0,0 +1,50 @@
+﻿namespace task_8
+{
+	public class TV
+	{
+		public string Vendor, Series, Model;
+		public string SerialNo;
+		public decimal ScreenSize;
+		public bool hasHDMI, hasT2;
+		public decimal Price;
+
+		public TV()
+		{
+			Vendor = "Unknown"; Series = ""; Model = "Generic";
+			SerialNo = "<not specified>";
+			ScreenSize = 15; Price = 0;
+            hasHDMI = false; hasT2 = false;
+		}
+
+		public TV(string Vendor, string Series, string Model, string SerialNo,
+            decimal ScreenSize, bool hasHDMI, bool hasT2, decimal Price)
+		{
+			this.Vendor = Vendor;
+			this.Series = Series;
+			this.Model = Model;
+			this.SerialNo = SerialNo;
+			this.ScreenSize = ScreenSize;
+			this.hasHDMI = hasHDMI;
+			this.hasT2 = hasT2;
+			this.Price = Price;
+		}
+
+		public string getFullName()
+		{
+			return $"{Vendor}, серія {Series}, модель {Model}\nСерійний номер: {SerialNo}";
+		}
+
+		public string getDescription()
+		{
+			return $"Діагональ екрану: {ScreenSize}, " +
+				(hasT2 ? "вбудований приймач Т2, " : "") +
+				(hasHDMI ? "порт HDMI для зовнішніх пристроїв" : "");
+		}
+
+		public decimal getPricePerInch()
+		{
+			return (decimal)Math.Round(Price / ScreenSize);
+		}
+	}
+}
+
