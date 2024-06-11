1c88561b3979bc203dcb261da22033b75bb34612
mikekondr <mike.kondr@gmail.com>
2024-06-12 02:27:26 +0300

    task 8

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -1,31 +1,42 @@
-﻿namespace task_8;
+﻿using task_8_lib;
+
+namespace task_8;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("\tВведіть дані ТВ");
-        Console.Write("Виробник: "); string? v = Console.ReadLine();
-        Console.Write("Модельний ряд: "); string? s = Console.ReadLine();
-        Console.Write("Модель: "); string? m = Console.ReadLine();
-        Console.Write("Серійний номер: "); string? sn = Console.ReadLine();
-        decimal d = getDecimal("Діагональ екрану: ");
-        decimal p = getDecimal("Ціна телевізора: ");
+        Console.Write("Яку кількість ТВ необхідно ввести? ");
+        int im = 0; int.TryParse(Console.ReadLine(), out im);
+        if (im == 0) System.Environment.Exit(0);
+        TV[] TVs = new TV[im];
 
-        Console.Write("Чи має ТВ приймач Т2? (y - так, n - ні): ");
-        ConsoleKeyInfo t = Console.ReadKey();
-        Console.WriteLine();
-        Console.Write("Чи має ТВ вхід HDMI? (y - так, n - ні): ");
-        ConsoleKeyInfo h = Console.ReadKey();
-        
-        TV myTV = new TV(v, s, m, sn, d,
-            (h.Key == ConsoleKey.Y), (t.Key == ConsoleKey.Y), p);
+        for (int i = 0; i < im; i++)
+        {
+            Console.WriteLine($"\n\n\tВведіть дані ТВ №{i+1}");
+            Console.Write("Виробник: "); string? v = Console.ReadLine();
+            Console.Write("Модельний ряд: "); string? s = Console.ReadLine();
+            Console.Write("Модель: "); string? m = Console.ReadLine();
+            Console.Write("Серійний номер: "); string? sn = Console.ReadLine();
+            decimal d = getDecimal("Діагональ екрану: ");
+            decimal p = getDecimal("Ціна телевізора: ");
+
+            Console.Write("Чи має ТВ приймач Т2? (y - так, n - ні): ");
+            ConsoleKeyInfo t = Console.ReadKey();
+            Console.WriteLine();
+            Console.Write("Чи має ТВ вхід HDMI? (y - так, n - ні): ");
+            ConsoleKeyInfo h = Console.ReadKey();
+
+            TVs[i] = new TV(v, s, m, sn, d,
+                (h.Key == ConsoleKey.Y), (t.Key == ConsoleKey.Y), p);
+        }
 
         Console.WriteLine("\n\n");
-        Console.WriteLine("Інформація про ТВ:");
-        Console.WriteLine(myTV.getFullName());
-        Console.WriteLine(myTV.getDescription());
-        Console.WriteLine($"Ціна за дюйм: {myTV.getPricePerInch()}");
+        Console.WriteLine("Інформація про ТВ:\n");
+        foreach (TV tv in TVs)
+        {
+            Console.WriteLine(tv.fullInfo + '\n');
+        }
     }
 
     private static decimal getDecimal(string s)
---
+++ task_8_lib/TV.cs
@@ -0,0 +1,54 @@
+﻿namespace task_8_lib;
+
+public class TV
+{
+    public string Vendor, Series, Model;
+    public string SerialNo;
+    public decimal ScreenSize;
+    public bool hasHDMI, hasT2;
+    public decimal Price;
+
+    public string fullInfo
+    {
+        get { return getFullName() + '\n' + getDescription() + '\n' +
+                $"Ціна за дюйм: {getPricePerInch()}"; }
+    }
+
+    public TV()
+    {
+        Vendor = "Unknown"; Series = ""; Model = "Generic";
+        SerialNo = "<not specified>";
+        ScreenSize = 15; Price = 0;
+        hasHDMI = false; hasT2 = false;
+    }
+
+    public TV(string Vendor, string Series, string Model, string SerialNo,
+        decimal ScreenSize, bool hasHDMI, bool hasT2, decimal Price)
+    {
+        this.Vendor = Vendor;
+        this.Series = Series;
+        this.Model = Model;
+        this.SerialNo = SerialNo;
+        this.ScreenSize = ScreenSize;
+        this.hasHDMI = hasHDMI;
+        this.hasT2 = hasT2;
+        this.Price = Price;
+    }
+
+    public string getFullName()
+    {
+        return $"{Vendor}, серія {Series}, модель {Model}\nСерійний номер: {SerialNo}";
+    }
+
+    public string getDescription()
+    {
+        return $"Діагональ екрану: {ScreenSize}, " +
+            (hasT2 ? "вбудований приймач Т2, " : "") +
+            (hasHDMI ? "порт HDMI для зовнішніх пристроїв" : "");
+    }
+
+    public decimal getPricePerInch()
+    {
+        return (decimal)Math.Round(Price / ScreenSize);
+    }
+}
